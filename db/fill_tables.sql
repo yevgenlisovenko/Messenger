116c0e999f70172f5d4a8ccc2156d6a1fddbb550
Yevgen Lisovenko <yevgen@yevgens-mac.local>
2022-05-16 21:35:37 -0400

    Changes to script for filling messages table with data

--- db/fill_tables.sql
+++ db/fill_tables.sql
@@ -5,9 +5,9 @@ INSERT INTO messages(date_time, sender_id, recipient_id, message) VALUES(NOW() -
 INSERT INTO messages(date_time, sender_id, recipient_id, message) VALUES(NOW() - INTERVAL '1 DAY - 3 HOUR', 1001, 2001, 'Short message ' || ceil(random() * 1000));
 INSERT INTO messages(date_time, sender_id, recipient_id, message) VALUES(NOW() - INTERVAL '1 DAY - 5 HOUR', 1001, 2001, 'Short message ' || ceil(random() * 1000));
 
-INSERT INTO messages(date_time, sender_id, recipient_id, message) VALUES(NOW() - INTERVAL '2 DAY', 1002, 2001, 'Short message ' || ceil(random() * 1000));
-INSERT INTO messages(date_time, sender_id, recipient_id, message) VALUES(NOW() - INTERVAL '29 DAY', 1002, 2001, 'Short message ' || ceil(random() * 1000));
-INSERT INTO messages(date_time, sender_id, recipient_id, message) VALUES(NOW() - INTERVAL '30 DAY', 1002, 2001, 'Short message ' || ceil(random() * 1000));
-INSERT INTO messages(date_time, sender_id, recipient_id, message) VALUES(NOW() - INTERVAL '31 DAY', 1002, 2001, 'Short message ' || ceil(random() * 1000));
-INSERT INTO messages(date_time, sender_id, recipient_id, message) VALUES(NOW() - INTERVAL '32 DAY', 1002, 2001, 'Short message ' || ceil(random() * 1000));
 INSERT INTO messages(date_time, sender_id, recipient_id, message) VALUES(NOW() - INTERVAL '33 DAY', 1002, 2001, 'Short message ' || ceil(random() * 1000));
+INSERT INTO messages(date_time, sender_id, recipient_id, message) VALUES(NOW() - INTERVAL '32 DAY', 1002, 2001, 'Short message ' || ceil(random() * 1000));
+INSERT INTO messages(date_time, sender_id, recipient_id, message) VALUES(NOW() - INTERVAL '31 DAY', 1002, 2001, 'Short message ' || ceil(random() * 1000));
+INSERT INTO messages(date_time, sender_id, recipient_id, message) VALUES(NOW() - INTERVAL '30 DAY', 1002, 2001, 'Short message ' || ceil(random() * 1000));
+INSERT INTO messages(date_time, sender_id, recipient_id, message) VALUES(NOW() - INTERVAL '29 DAY', 1002, 2001, 'Short message ' || ceil(random() * 1000));
+INSERT INTO messages(date_time, sender_id, recipient_id, message) VALUES(NOW() - INTERVAL '2 DAY', 1002, 2001, 'Short message ' || ceil(random() * 1000));
